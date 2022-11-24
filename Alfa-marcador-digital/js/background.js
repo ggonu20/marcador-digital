8f5a1a32216e186da422049c15e238b90b86ac78
ggonu20 <g.gonzalezn@utem.cl>
2022-11-24 19:42:10 -0300

    a

--- Alfa-marcador-digital/js/background.js
+++ Alfa-marcador-digital/js/background.js
@@ -1,8 +1,10 @@
 
-chrome.notifications.create({
+chrome.action.onClicked.addListener(() =>{
+    chrome.notifications.create({
     type: 'basic',
     iconUrl: '../icons/icon_128.png', //icono
     title: '', //titulo
     message: '', //mensaje
     priority: 0
+    });
 });
--- Alfa-marcador-digital/js/popup.js
+++ Alfa-marcador-digital/js/popup.js
@@ -1,15 +1,5 @@
-function notificar(event){
-    const t1 =event.URL;
-    chrome.notifications.create({
-        type: 'basic',
-        iconUrl: '../icons/icon_128.png', //icono
-        title: 'Hola', //titulo
-        message: 'Mundo'
-    });
-    window.close();
-}
 
-document.getElementById('button_').addEventListener('click',notificar);
+document.getElementById('button_').addEventListener('click');
 
 
   
